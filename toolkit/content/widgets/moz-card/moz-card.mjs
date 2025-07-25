1408e6239718f96ac4e26893ac500303387cb0da
Brian Grinstead <briangrinstead@gmail.com>
2025-07-25 15:00:48 -0700

    stash

--- toolkit/content/widgets/moz-card/moz-card.mjs
+++ toolkit/content/widgets/moz-card/moz-card.mjs
@@ -7,8 +7,8 @@ import {
   html,
   ifDefined,
   when,
-} from "../vendor/lit.all.mjs";
-import { MozLitElement } from "../lit-utils.mjs";
+} from "chrome://global/content/vendor/lit.all.mjs";
+import { MozLitElement } from "chrome://global/content/lit-utils.mjs";
 
 /**
  * Cards contain content and actions about a single subject.
